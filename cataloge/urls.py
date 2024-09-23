fc220028a87d0be0126a7922e60d02cbd19dc072
ahmed <ahmedsamer6788@gmail.com>
2024-09-23 21:07:53 +0300

    upload files

---
+++ cataloge/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("create-book", views.BookCreate.as_view(), name="create_book"),
+    path("detail/<int:pk>", views.BookDetail.as_view(), name="detail"),
+    path("accounts/register", views.Register.as_view(), name="register"),
+
+]
